e265a1ee68a401c0fb003d6f73557ee1ed21a4d7
Muamer Ribica <muamer.ribica@gmail.com>
2013-11-24 15:58:31 +0100

    add class and instance method for generating short_url

--- lib/short_url.rb
+++ lib/short_url.rb
@@ -1,3 +1,20 @@
+require 'short_url/exceptions'
+require 'short_url/encoder'
+
 module ShortUrl
-  require 'short_url/exceptions'
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  def short_url
+    raise ObjectHasNoId unless id
+    Encoder.new.encode_url(id)
+  end
+
+  module ClassMethods
+    def generate_short_url(id)
+      Encoder.new.encode_url(id)
+    end
+  end
 end
+
